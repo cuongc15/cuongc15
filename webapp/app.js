c71a9358ab1bae66369123e0aa5080da2e1babf6
cuongc15 <69023803+cuongc15@users.noreply.github.com>
2026-05-21 10:27:32 +0700

    Update app.js

--- webapp/app.js
+++ webapp/app.js
@@ -1,4 +1,4 @@
-const API_BASE = window.API_BASE || 'http://127.0.0.1:8000';
+const API_BASE = window.API_BASE || 'postgresql://postgres:xVFOrWrHXkYlYanwDNmaRvmcQbJayoOd@kodama.proxy.rlwy.net:44666/railway';
 
 const classForm = document.getElementById('classForm');
 const assignmentForm = document.getElementById('assignmentForm');
