01f68c5b42a039a8bdcd83dde999c829ef254126
cuongc15 <69023803+cuongc15@users.noreply.github.com>
2026-05-21 10:27:59 +0700

    Update config.js

--- webapp/config.js
+++ webapp/config.js
@@ -1,3 +1,3 @@
 // Đổi API_BASE khi deploy frontend lên hosting tĩnh miễn phí.
 // Ví dụ: https://lms-api.onrender.com hoặc domain Railway public URL.
-window.API_BASE = 'http://127.0.0.1:8000';
+window.API_BASE = 'postgresql://postgres:xVFOrWrHXkYlYanwDNmaRvmcQbJayoOd@kodama.proxy.rlwy.net:44666/railway';
